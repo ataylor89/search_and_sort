2db1a2277248460ece702d57bb71240c3d2e36b7
Andrew Taylor <rolyata47@gmail.com>
2026-07-25 11:35:49 -0500

    Adding comments

--- sort.py
+++ sort.py
@@ -8,8 +8,12 @@ def partition(arr, low, high):
     pivot = arr[high]
     i = low - 1
     for j in range(low, high):
+        # If you can swap it, you swap it
+        # That is, if you can swap the elements at indices i and j, swap them
         if arr[j] <= pivot:
             i += 1
             arr[i], arr[j] = arr[j], arr[i]
+    # i + 1 is the index after the last swap
+    # (Substitute low - 1 + n for i where n is the number of swaps we performed)
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
     return i + 1
