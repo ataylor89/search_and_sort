daf83f04c20ef597bc2ec4d65d624a7140350b4d
Andrew Taylor <rolyata47@gmail.com>
2026-07-25 11:47:37 -0500

    Adding comments

--- sort.py
+++ sort.py
@@ -16,5 +16,6 @@ def partition(arr, low, high):
     # i + 1 is the index after the last swap
     # (Substitute low - 1 + n for i where n is the number of swaps we performed)
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    # Now, every element from index low to index i is less than or equal to our pivot, and every element from index i+2 to index high is greater than our pivot
+    # Now, every element from index low to index i is less than or equal to our pivot,
+    # and every element from index i+2 to index high is greater than our pivot
     return i + 1
