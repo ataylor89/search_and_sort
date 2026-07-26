2b4d3c69940c8ba39d75614e960994074aa2fd99
Andrew Taylor <rolyata47@gmail.com>
2026-07-25 19:33:07 -0500

    Updating file

--- test_sort.py
+++ test_sort.py
@@ -2,18 +2,18 @@ from sort import quicksort, simplesort
 from time import perf_counter
 from random import randint
 
-def test(r, a, b, n, qs=True, ss=True, show=False, newline=False):
+def test(r, a, b, n, qs=True, ss=True, showlist=False, newline=False):
     print('Round %d (a=%d b=%d n=%d)' %(r, a, b, n))
     a, b, n = int(a), int(b), int(n)
     arr = [randint(a, b) for i in range(n)]
-    if show:
+    if showlist:
         print('Unsorted list: %s' %arr)
     if qs:
         arr1 = arr.copy()
         start_time = perf_counter()
         quicksort(arr1, 0, len(arr1) - 1)
         quicksort_time = perf_counter() - start_time
-        if show:
+        if showlist:
             print('Quicksort result: %s' %arr1)
         print(f'Quicksort execution time: {quicksort_time:.6f} seconds')
     if ss:
@@ -21,7 +21,7 @@ def test(r, a, b, n, qs=True, ss=True, show=False, newline=False):
         start_time = perf_counter()
         simplesort(arr2)
         simplesort_time = perf_counter() - start_time
-        if show:
+        if showlist:
             print('Simplesort result: %s' %arr2)
         print(f'Simplesort execution time: {simplesort_time:.6f} seconds')
     if qs and ss:
@@ -30,7 +30,7 @@ def test(r, a, b, n, qs=True, ss=True, show=False, newline=False):
         print('')
 
 if __name__ == '__main__':
-    test(1, 0, 20, 20, show=True, newline=True)
+    test(1, 0, 20, 20, showlist=True, newline=True)
     test(2, 0, 100, 100, newline=True)
     test(3, 0, 1000, 1000, newline=True)
     test(4, 0, 1e4, 1e4, newline=True)
