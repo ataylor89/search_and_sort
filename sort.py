97ddb06696987c6e47e1cb137a2484c6b7af0fa0
Andrew Taylor <rolyata47@gmail.com>
2026-08-07 21:44:38 -0500

    Simplifying the partition method

--- sort.py
+++ sort.py
@@ -32,18 +32,15 @@ def quicksort(arr, low, high):
 def partition(arr, low, high):
     i = low - 1
     pivot = arr[high]
-    for j in range(low, high):
+    for j in range(low, high + 1):
         # If you can swap it, you swap it
         if arr[j] <= pivot:
             i += 1
             arr[i], arr[j] = arr[j], arr[i]
-    # i + 1 is the index after the last swap, if there was a swap
-    # (Substitute low - 1 + n for i where n is the number of swaps we performed)
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    # Now, every element from index low to index i is less than or equal to our pivot,
-    # every element from index i + 2 to index high is greater than our pivot,
-    # and our pivot is stored at index i + 1
-    return i + 1
+    # Now, every element from index low to index i - 1 is less than or equal to our pivot,
+    # every element from index i + 1 to index high is greater than our pivot,
+    # and our pivot is stored at index i
+    return i
 
 # We are going to include a simpler, less efficient sorting algorithm for comparison
 def simplesort(arr):
